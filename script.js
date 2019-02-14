7830657874da934560838b78725a6ad8cf70d246
Joana Souza <joo.souza09@hotmail.com>
2019-02-14 21:36:12 -0200

    Codificar o JavaScript para formar uma escadinha conforme gosto do usuário

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let linhas = Number(prompt('Por favor digite quantidade de linhas da escada:'));
+let material = prompt('Por favor digite material da escada:');
+let escada = "";
+
+for (i = 0; i < linhas; i++) {
+escada = escada+material;
+console.log(escada)
+}
